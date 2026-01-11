b9f82990bccf15fc3972953d8a26731236fa83d8
Darfresor <vectnik@gmail.com>
2026-01-11 13:53:49 +0300

    доработка поля даты последнего обновления в film

--- src/main/java/com/javarush/ostapenko/hibernate_2/domain/entity/Film.java
+++ src/main/java/com/javarush/ostapenko/hibernate_2/domain/entity/Film.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Types;
@@ -58,6 +59,7 @@ public class Film {
     @JdbcTypeCode(Types.CHAR)
     private String specialFeatures;
     @Column(name = "last_update")
+    @UpdateTimestamp
     private Date lastUpdate;
 
     @ManyToMany(fetch = FetchType.LAZY)
